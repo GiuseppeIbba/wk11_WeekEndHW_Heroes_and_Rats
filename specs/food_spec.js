71cde3a7af2a2b485edf90d122e8a7e0d1edd3d0
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-18 20:57:42 +0000

    added food.replenishmentValue test on food_spec.js

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -12,4 +12,8 @@ describe("Food Tests", function(){
   it("should have a name", function(){
     assert.strictEqual(food.name, "Blackberry");
   })
+
+  it("should ha a replenishment value", function(){
+    assert.strictEqual(food.replenishmentValue, 1);
+  })
 })
